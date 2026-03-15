ae7992ac836a646dc7398452ea9a1582f3dcde7a
Ryanpratama01 <220441100098@student.trunojoyo.ac.id>
2026-03-16 05:33:14 +0700

    refresh all

--- config/database.php
+++ config/database.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -44,7 +44,8 @@ return [
         ],
 
         'mysql' => [
-            'url' => env('DATABASE_URL'),
+            'driver' => 'mysql',
+            'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
